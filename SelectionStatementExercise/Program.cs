ed29916f4690ca2b3bec912f60e09c620b5bef83
Dan Marino <technicaldanny302@gmail.com>
2024-06-29 23:25:14 -0700

    completed exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            //Number Guessing Game
+            var r = new Random();
+            //var favNumber = r.Next(1, 1000);
+            var favNumber = 5;
+            int userInput;
+            var counter = 0;
+
+            do
+            {
+
+                Console.WriteLine("Pick a number from 1 - 1000");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too low");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                counter++;
+
+                Console.WriteLine($"You have guessed{counter} times!");
+            }
+            while (userInput != favNumber);
+
+            //Switch-case
+            Console.WriteLine($"What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower)
+            {
+                case "Math":
+                    Console.WriteLine("I hate math!");
+                    break;
+                case "science":
+                    Console.WriteLine($"I love science");
+                    break;
+                case "pe":
+                    Console.WriteLine("PE is the best!");
+                    break;
+                case "history":
+                    Console.WriteLine("English is boring");
+                    break;
+                case "english":
+                    Console.WriteLine("English is boring");
+                    break;
+                default:
+                    Console.WriteLine("I dont know that subject");
+                    break;
+            }
     }
 }
